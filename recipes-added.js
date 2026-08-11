cd7908f0ed50e57186190d9f4fdd2cbaa670a9e5
frankgbp-afk <frank.peterswi@gmail.com>
2026-08-10 19:38:36 -0500

    Add Beth's SOS Mix recipe

---
+++ recipes-added.js
@@ -0,0 +1,26 @@
+recipes.push({
+  id: "beth-sos-mix",
+  name: "SOS Mix",
+  category: "Sides",
+  author: "Beth",
+  description: "A make-ahead soup-or-sauce mix that can replace a can of cream soup in recipes.",
+  prepTime: "5 min",
+  cookTime: "Varies",
+  totalTime: "5 min + cooking when used",
+  servings: "Varies",
+  tags: ["side", "soup base", "sauce base", "cream soup", "pantry", "make ahead"],
+  ingredients: [
+    "2 cups non-fat dry milk",
+    "3/4 cup cornstarch",
+    "1/4 cup instant chicken bouillon",
+    "2 tablespoons dried onion flakes",
+    "2 teaspoons Italian seasoning (optional)"
+  ],
+  instructions: [
+    "Stir all ingredients together and store in an airtight container.",
+    "To replace one can of cream soup, add 1/3 cup SOS Mix and 1 1/4 cups (10 ounces) water to a saucepan and whisk to combine.",
+    "Heat on the stovetop, whisking as needed, until thickened.",
+    "Add the thickened cream soup mixture to your recipe."
+  ],
+  notes: "SOS stands for ‘soup or sauce.’ Use 1/3 cup of the dry mix with 1 1/4 cups water to replace one can of cream soup."
+});
