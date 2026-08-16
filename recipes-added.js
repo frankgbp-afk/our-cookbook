122c638233f6d8e8c3ed84eb85103bff2b320fa6
frankgbp-afk <frank.peterswi@gmail.com>
2026-08-16 10:13:30 -0500

    Add Beth's White Chicken Chili recipe

--- recipes-added.js
+++ recipes-added.js
@@ -25,3 +25,44 @@ recipes.push({
   ],
   notes: "SOS stands for ‘soup or sauce.’ Use 1/3 cup of the dry mix with 1 1/4 cups water to replace one can of cream soup."
 });
+
+recipes.push({
+  id: "beth-white-chicken-chili",
+  name: "White Chicken Chili",
+  category: "Soups",
+  image: "beth-white-chicken-chili.jpg",
+  author: "Beth",
+  description: "A creamy white chicken chili with green chiles, corn, white beans, warm spices, and Neufchâtel cheese.",
+  prepTime: "20 min",
+  cookTime: "36 min",
+  totalTime: "56 min",
+  servings: "Varies",
+  tags: ["chili", "soup", "chicken", "white beans", "corn", "green chiles"],
+  ingredients: [
+    "1 pound boneless skinless chicken breasts, diced into 1/2-inch pieces (or substitute rotisserie chicken)",
+    "1 small yellow onion, diced",
+    "1 tablespoon olive oil",
+    "2 cloves garlic, minced",
+    "2 (14.5 ounce) cans chicken broth or bone broth",
+    "1 (4 ounce) can diced green chiles",
+    "1 1/2 teaspoons cumin",
+    "3/4 teaspoon paprika",
+    "1/2 teaspoon oregano",
+    "1/2 teaspoon coriander",
+    "1/4 teaspoon cayenne pepper",
+    "Salt and fresh ground pepper, to taste",
+    "8 ounces Neufchâtel cheese, cut into 12 slices",
+    "1 1/4 cups corn",
+    "2 (15 ounce) cans cannellini or great northern beans, drained and rinsed",
+    "1 tablespoon fresh lime juice",
+    "Chopped fresh cilantro, shredded Monterey Jack cheese, tortilla chips, and sour cream for garnish (optional)"
+  ],
+  instructions: [
+    "Heat the oil over medium-high heat. Cook the chicken and onion until the chicken is no longer pink, about 6 minutes.",
+    "Add the garlic and sauté 30 seconds longer.",
+    "Add the chicken broth, green chiles, cumin, paprika, oregano, coriander, cayenne, salt, and pepper. Bring the mixture just to a boil, then reduce the heat and simmer for 15 minutes.",
+    "Add the Neufchâtel cheese and stir until nearly melted. Stir in the corn and 1 can of the beans.",
+    "In a food processor, purée the remaining can of beans with 1/4 cup of broth from the soup until smooth, then add it to the soup. This step can be skipped, but the soup will not be as creamy.",
+    "Simmer 15 minutes longer. Mix in the fresh lime juice and top with desired garnishes."
+  ]
+});
